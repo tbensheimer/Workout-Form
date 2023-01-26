657719d5c48db1068d8e4fb3d87dade51678da3e
tbensheimer <104734721+tbensheimer@users.noreply.github.com>
2023-01-26 18:53:47 -0500

    Add files via upload

---
+++ controllers/workoutControllers.js
@@ -0,0 +1,65 @@
+import Workout from "../models/Workout"
+
+const getAllWorkouts = (req, res) => {
+const workouts = Workout.find()
+.then(() => {
+    res.json(workouts);
+})
+.catch((err) => {
+    console.log(err);
+})
+}
+
+const getWorkoutDetails = (req, res) => {
+    const id = req.params;
+    const workout = Workout.findById(id)
+    .then(() => {
+        res.json(workout);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+}
+
+const createWorkout = (req, res) => {
+const workout = req.body;
+const workoutCreated = Workout.create((workout))
+.then(() => {
+    res.json(workoutCreated);
+})
+.catch((err) => {
+    console.log(err);
+})
+}
+
+const updateWorkout = (req, res) => {
+const id = req.params;
+const body = req.body;
+const workout = Workout.findByIdAndUpdate(id, body)
+.then(() => {
+    res.json(workout);
+})
+.catch((err) => {
+    console.log(err);
+})
+}
+
+const deleteWorkout = (req, res) => {
+const id = req.params;
+const workout = Workout.findByIdAndDelete(id)
+.then(() => {
+    res.json(workout);
+})
+.catch((err) => {
+    console.log(err);
+})
+}
+
+module.exports = {
+    deleteWorkout,
+    updateWorkout,
+    createWorkout,
+    getAllWorkouts,
+    getWorkoutDetails
+}
