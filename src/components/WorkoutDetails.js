b03608ff707970539aaf4240de4f38b586a3a5ff
tbensheimer <104734721+tbensheimer@users.noreply.github.com>
2023-01-30 20:23:51 -0500

    Update WorkoutDetails.js

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,4 +1,26 @@
+import { useDispatch } from "react-redux"
+import { deleteWorkout, toggleEdit, getWorkoutDetails } from "../features/workoutSlice";
+
 export default function WorkoutDetails({workout}) {
+    const dispatch = useDispatch();
+
+    const handleDeleteClick = async () => {
+        console.log(workout._id);
+        const response = await fetch("/api/workouts/" + workout._id, {
+            method: "DELETE"
+        });
+        const data = await response.json();
+
+        if(response.ok) {
+            dispatch(deleteWorkout(data));
+        }
+    }
+
+    const handleEditClick = () => {
+        dispatch(toggleEdit());
+        dispatch(getWorkoutDetails(workout));
+    }
+
 
     return (
         <div className="workout-details">
@@ -6,6 +28,8 @@ export default function WorkoutDetails({workout}) {
             <p><strong>Load (kg):</strong> {workout.load}</p>
             <p><strong>Reps: </strong> {workout.reps}</p>
             <p>{workout.createdAt}</p>
+            <button class="edit" type="button" onClick={handleEditClick}>edit</button>
+            <button class="delete" type="button" onClick={handleDeleteClick}>delete</button>
         </div>
     )
 }
