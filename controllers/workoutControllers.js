029b269e8bba0a08f105796adc0e5fa2cea51b19
tbensheimer <104734721+tbensheimer@users.noreply.github.com>
2023-01-30 20:20:35 -0500

    Update workoutControllers.js

--- controllers/workoutControllers.js
+++ controllers/workoutControllers.js
@@ -1,59 +1,75 @@
 const Workout = require("../models/Workout");
+const mongoose = require("mongoose");
 
-const getAllWorkouts = (req, res) => {
-const workouts = Workout.find()
-.then(() => {
-    res.json(workouts);
-})
-.catch((err) => {
-    console.log(err);
-})
+const getAllWorkouts = async (req, res) => {
+    try {
+        const workouts = await Workout.find().sort({ createdAt: -1})
+        res.status(200).json(workouts);
+    }
+    catch (err) {
+        res.status(400).json({error: err.message})
+    }
 }
 
-const getWorkoutDetails = (req, res) => {
-    const id = req.params;
-    const workout = Workout.findById(id)
-    .then(() => {
-        res.json(workout);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+const getWorkoutDetails = async (req, res) => {
+    const id = mongoose.Types.ObjectId(req.params.id.trim());
 
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({error: "No such workout with given Id"});
+    }
+
+    const workout = Workout.findById(id);
+
+    if (workout) {
+        res.status(200).json(workout);
+    }
+    else {
+        res.status(400).json({error: err.message});
+    }
 }
 
-const createWorkout = (req, res) => {
-const workout = req.body;
-const workoutCreated = Workout.create((workout))
-.then(() => {
-    res.json(workoutCreated);
-})
-.catch((err) => {
-    console.log(err);
-})
+const createWorkout = async (req, res) => {
+    const {title, load, reps} = req.body;
+try {
+    const workout = await Workout.create({title, load, reps});
+    res.status(200).json(workout)
+}
+catch(err) {
+    res.status(400).json({error: err.message});
+}
 }
 
-const updateWorkout = (req, res) => {
-const id = req.params;
-const body = req.body;
-const workout = Workout.findByIdAndUpdate(id, body)
-.then(() => {
-    res.json(workout);
-})
-.catch((err) => {
-    console.log(err);
-})
+const updateWorkout = async (req, res) => {
+const id = mongoose.Types.ObjectId(req.params.id.trim());
+const updatedBody = req.body;
+
+if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({error: "No such workout with given Id"});
+}
+const workout = await Workout.findByIdAndUpdate(id, updatedBody)
+
+if(workout) {
+    res.status(200).json(workout);
+}
+else {
+    res.status(400).json({error: "There was an error updating the workout"});
+}
 }
 
-const deleteWorkout = (req, res) => {
-const id = req.params;
-const workout = Workout.findByIdAndDelete(id)
-.then(() => {
-    res.json(workout);
-})
-.catch((err) => {
-    console.log(err);
-})
+const deleteWorkout = async (req, res) => {
+const id = mongoose.Types.ObjectId(req.params.id.trim());
+
+if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({error: "No such workout with given Id"});
+}
+
+const workout = await Workout.findByIdAndDelete(id)
+if(workout) {
+    res.status(200).json(workout);
+}
+else {
+    res.status(400).json({error: "There was an error deleting workout"});
+}
 }
 
 module.exports = {
