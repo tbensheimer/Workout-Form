782c73252b3b162f148b199adf230c64afaf44ea
tbensheimer <104734721+tbensheimer@users.noreply.github.com>
2023-02-06 22:45:44 -0500

    Added links to login and signup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,12 @@ export default function Navbar() {
             <div className="container">
                 <Link to="/"><h1>Workout Buddy</h1></Link>
             </div>
+        <nav>
+                <div>
+                    <Link to="/signup">Sign up</Link>
+                    <Link to="/login">Log in</Link>
+                </div>
+            </nav>
         </header>
     )
 }
