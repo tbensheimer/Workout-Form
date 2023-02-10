a0ea10b42d61e1ea27bd1ac8d266472a21cac6b6
tbensheimer <104734721+tbensheimer@users.noreply.github.com>
2023-02-09 21:06:52 -0500

    added user state and authorization headers

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,14 +1,18 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { deleteWorkout, toggleEdit, getWorkoutDetails } from "../features/workoutSlice";
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 export default function WorkoutDetails({workout}) {
     const dispatch = useDispatch();
+    const user = useSelector(state => state.user);
 
     const handleDeleteClick = async () => {
         console.log(workout._id);
         const response = await fetch("/api/workouts/" + workout._id, {
-            method: "DELETE"
+            method: "DELETE",
+            headers: {
+                'Authorization': `Bearer ${user.token}`
+            }
         });
         const data = await response.json();
 
@@ -21,6 +25,19 @@ export default function WorkoutDetails({workout}) {
         dispatch(toggleEdit());
         dispatch(getWorkoutDetails(workout));
     }
+
+
+    return (
+        <div className="workout-details">
+            <h4 className="workout-title">{workout.title}</h4>
+            <p><strong>Load (kg):</strong> {workout.load}</p>
+            <p><strong>Reps: </strong> {workout.reps}</p>
+            <p>{formatDistanceToNow(new Date(workout.createdAt), {suffix: true})} ago</p>
+            <button className="edit" type="button" onClick={handleEditClick}>edit</button>
+            <button className="delete" type="button" onClick={handleDeleteClick}>delete</button>
+        </div>
+    )
+}
 
 
     return (
